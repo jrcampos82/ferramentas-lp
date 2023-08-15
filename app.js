854abf99808ceec8a238d8775555fe4475bffa9a
Beto <jrcampos82@gmail.com>
2023-08-14 22:23:47 -0400

    CRUD api products

--- app.js
+++ app.js
@@ -6,20 +6,21 @@ const port = 3000;
 // deconding / enconding
 app.use(express.urlencoded({ extended: true }));
 
-const products = [
+let products = [
   {
-    id: 1,
+    id: 0,
     name: "Product 1",
   },
   {
-    id: 2,
+    id: 1,
     name: "Product 2",
   },
   {
-    id: 3,
+    id: 2,
     name: "Product 3",
   },
 ];
+
 /**
  * Configuração de rotas
  * GET, PUT, DELETE, POST
@@ -34,6 +35,26 @@ app.get("/api", (req, res) => {
   res.json(products);
 });
 
+app.get("/api/:id", (req, res) => {
+  // recebe o param id
+  res.send("ID = " + req.params.id); // exibe o param id
+});
+
+app.get("/api/products/:id", (req, res) => {
+  const id = Number(req.params.id); // falta de converter em number (pode acontecer problem)
+
+  const product = products.find((p) => p.id === id);
+
+  // if (!product) return res.status(404).send("Product not found!");
+  if (!product)
+    return res.status(404).json({
+      status: 404,
+      message: "Product not found!",
+    });
+
+  res.json(product);
+});
+
 app.post("/api", (req, res) => {
   let name = req.body.name;
   console.log(`Name iss ${name}`);
@@ -42,6 +63,49 @@ app.post("/api", (req, res) => {
 
   return res.json(products);
 });
+
+/**
+ * FUNCAO UPDATE
+ */
+app.put("/api/products/:id", (req, res) => {
+  // let name = req.body.name
+  let { name } = req.body;
+  let id = Number(req.params.id);
+
+  const product = products.find((p) => p.id === id);
+
+  if (!product)
+    return res.status(404).json({
+      status: 404,
+      message: "Product not found!",
+    });
+
+  products[id].name = name;
+
+  return res.json(products[id]);
+});
+/**
+ * DELETE FUNCTION
+ */
+app.delete("/api/products/:id", (req, res) => {
+  let id = Number(req.params.id);
+
+  const product = products.find((p) => p.id === id);
+
+  if (!product)
+    return res.status(404).json({
+      status: 404,
+      message: "Product not found!",
+    });
+
+  products = products.filter((p) => p.id !== id);
+
+  return res.json({
+    status: 204,
+    message: "Product deleted!",
+  });
+});
+
 
 /**
  * Configuração da porta
